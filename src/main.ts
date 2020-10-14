2a2a36936606250e72ed644ace74c34623341397
Arthur Henrique Eggert <opa.roa@gmail.com>
2020-10-14 12:22:55 +0200

    create base scrapper for the guild page

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,88 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { Browser, launch, Page } from 'puppeteer';
+import { load } from 'cheerio';
+import { format, startOfToday, startOfYesterday, subDays } from 'date-fns';
+
+type UserAction = {
+  who: string;
+  when: string;
+  what: string;
+  link: string;
+};
+
+function buildURL(link: string): string {
+  const ROOT_URL = 'https://www.dofus.com';
+  return `${ROOT_URL}${link}`;
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+function buildCosaGuildURL(): string {
+  const URl_COSA = `/pt/mmorpg/comunidade/anuarios/paginas-guildas/201900202-cosa-nostra`;
+  return buildURL(URl_COSA);
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+async function getContent(page: Page, url: string): Promise<string> {
+  await page.goto(url);
+  return page.content();
 }
+
+function getEventsHtmlContent(html: string): string[] {
+  const selector = `div[class="ak-character-actions"] > 
+                    div[class="ak-actions"] > 
+                    div[class="ak-actions-list"] > 
+                    div[class*="ak-container ak-content-list"] > 
+                    div[class="ak-list-element"]`;
+
+  const $ = load(html);
+  const events: string[] = [];
+  $(selector).each(function (_, element) {
+    const event: string = $(element).html() as string;
+    events.push(event);
+  });
+  return events;
+}
+
+function transformHtmlToEvent(events: string[]): UserAction[] {
+  const transformTextToDate = (when: string): string => {
+    let date = null;
+    if (when.includes('hoje')) {
+      date = startOfToday();
+    } else if (when.includes('ontem')) {
+      date = startOfYesterday();
+    } else {
+      const daysAgo = Number(when.replace(/\D/g, ''));
+      date = subDays(startOfToday(), daysAgo);
+    }
+    return format(date, 'dd/MM/yyyy');
+  };
+
+  return events.map((event) => {
+    const $ = load(event);
+    const when = transformTextToDate($('span[class="date"]').text());
+    const action = $('a[class="lien_action"]');
+    const who = action.text();
+    const link = `${buildURL(action.attr('href') as string)}`;
+    const what = $('.ak-title').text().includes('uniu') ? 'ENTROU' : 'SAIU';
+
+    return {
+      who,
+      when,
+      what,
+      link,
+    };
+  });
+}
+
+async function main() {
+  const browser: Browser = await launch();
+  try {
+    const page: Page = await browser.newPage();
+    const content: string = await getContent(page, buildCosaGuildURL());
+    const events = transformHtmlToEvent(getEventsHtmlContent(content));
+    console.log(events);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await browser.close();
+  }
+}
+
+(async () => await main())();
